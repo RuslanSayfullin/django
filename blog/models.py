d95d20b8a29bc5160f4f147a81047e1ce2af28b8
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-05-02 00:01:06 +0500

    Создание сайта администрирования для моделей

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    autor = models.ForeignKey(User,
+    author = models.ForeignKey(User,
                               on_delete=models.CASCADE,
                               related_name='blog_posts')
     body = models.TextField()
@@ -27,5 +27,5 @@ class Post(models.Model):
             models.Index(fields=['-publish'])
         ]
 
-    def __str__():
+    def __str__(self):
         return self.title 
