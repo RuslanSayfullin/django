322d8dc5fec4973d9c7d155e09250a799d375bcf
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-05-10 22:42:54 +0500

    Создание дружественных для поисковой оптимизации URL-адресов постов

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.http import Http404
+
 
 def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
-
-    return render(request, "blog/post/detail.html", {'post': post})
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
