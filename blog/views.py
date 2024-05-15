4da1007b99458029246f84f8bf7de87dd0f3a629
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-05-15 10:35:23 +0500

    Добавление комментариев в представление детальной информации о посте

--- blog/views.py
+++ blog/views.py
@@ -32,7 +32,15 @@ def post_detail(request, year, month, day, post):
         publish__year=year,
         publish__month=month,
         publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # Список активных комментариев к этому посту
+    comments = post.comments.filter(active=True)
+    # Форма для комментирования пользователями
+    form = CommentForm()
+    return render(request, 
+        'blog/post/detail.html',
+        {'post': post,
+        'comments': comments,
+        'form': form})
 
 class PostListView(ListView):
     """
